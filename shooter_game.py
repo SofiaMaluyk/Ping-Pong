7d5bd8a92c956e8cf52152a61b917be464985a61
SofiaMaluyk <147834462+SofiaMaluyk@users.noreply.github.com>
2023-10-17 17:33:14 +0200

    Update shooter_game.py
    Создание окна. Создание спрайтов. Движение спрайтов с помощью клавиш. Создание мяча и его движение. Проигрыш игрока.

--- shooter_game.py
+++ shooter_game.py
@@ -9,6 +9,8 @@ y1 = 65
 
 clock = time.Clock()
 FPS = 60
+font.init()
+
 
 
 class GameSprite(sprite.Sprite):
@@ -39,34 +41,64 @@ class Player(GameSprite):
 
 sprite1 = Player('platform.png', 70, 100, 5, 20, 250)
 sprite2 = Player('platform.png', 600, 100, 5, 20, 250)
+ball = GameSprite('ball_1615463127.png', 100, 100, 2, 60, 60)
 
+speed_x = 2
+speed_y = 2
+finish = False
+
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
 
 run = True
 while run:
-    window.blit(galaxy,(0,0))
-
     for e in event.get():
         if e.type == QUIT:
             run = False
+
+
+    window.blit(galaxy,(0,0))
+
+   
+    if finish != True:
+       ball.rect.x += speed_x
+       ball.rect.y += speed_y
+
+    if ball.rect.y > 450 or ball.rect.y <0:
+        speed_y *= -1
+
+    if sprite.collide_rect(sprite1, ball) or sprite.collide_rect(sprite2, ball):
+        speed_x *= -1
+
+    if ball.rect.x < 0:
+        finish = True
+        window.blit(lose1, (200, 200))
+
+    if ball.rect.x > 700:
+        finish = True
+        window.blit(lose1, (200, 200))
+
+
+
+    sprite1.resent()
+    sprite1.update_l()
+
+    sprite2.resent()
+    sprite2.update_r()
+
+    ball.resent()
+    ball.update()
+    
         
 
        
 
             
 
-    sprite1.resent()
-    sprite1.update()
-
-    sprite2.resent()
-    sprite2.update()
+    
         
 
 
     display.update()
     clock.tick(FPS)
-
-
-
-
-
-
